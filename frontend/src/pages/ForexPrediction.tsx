099f248d762b2d5ed3fc82b8f41dc90888f5d4b6
Hasitha <hasithapeiris.me@gmail.com>
2024-12-03 02:26:41 +0530

    fix: update base URL

--- frontend/src/pages/ForexPrediction.tsx
+++ frontend/src/pages/ForexPrediction.tsx
@@ -36,7 +36,7 @@ const ForexPrediction: React.FC = () => {
 
     try {
       const response = await axios.post<PredictionResult>(
-        "http://localhost:5000/predict",
+        "https://tea-plantation-portal.onrender.com/predict",
         formData
       );
       setPrediction(response.data);
