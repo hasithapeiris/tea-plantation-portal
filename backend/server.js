1001b24a2a97a16a2b19f59851e30fe40daf2e05
Hasitha <hasithapeiris.me@gmail.com>
2025-03-13 11:24:18 +0530

    feat: connect backend with mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cors from "cors";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+
+// Middleware for parsing incoming requests with json payloads
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
